31ce76ea384f54e78a4935528113030e00d61ecf
Corbin, Brandon <bcorbin@fusionalliance.com>
2019-05-10 20:04:06 -0400

    Storage
    - Adding notes

--- src/lib/storage/storage.js
+++ src/lib/storage/storage.js
@@ -1,4 +1,15 @@
+/**
+ * A simple storage system using localstorage.
+ * let store = new Storage('my-storage-place');
+ * store.set('name','Papa Smurf')
+ * store.get('name')  // Papa Smurf
+ */
+
 module.exports = class Storage {
+    /**
+     * Give it a name for the top level store
+     * @param {String} storeName
+     */
     constructor(storeName) {
         this.storeName = storeName || `store-${Math.random()}`;
         try {
@@ -9,15 +20,28 @@ module.exports = class Storage {
         }
     }
 
+    /**
+     * Set a key, value
+     * @param {String} key
+     * @param {Any} value
+     */
     set(key, value) {
         this.data[key] = value;
         return localStorage.setItem(this.storeName, JSON.stringify(this.data));
     }
 
+    /**
+     * Get a Value by Key
+     * @param {String} key
+     */
     get(key) {
         return this.data[key];
     }
 
+    /**
+     * Remove a Key
+     * @param {String} key
+     */
     remove(key) {
         delete this.data[key];
         return this;
